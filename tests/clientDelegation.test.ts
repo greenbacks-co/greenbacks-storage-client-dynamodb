3eef6450a8e65c01ab181a5d2cb296b40e892414
taylor <taylor.laekeman@gmail.com>
2021-07-11 16:02:17 -0400

    add item reader to client

--- tests/clientDelegation.test.ts
+++ tests/clientDelegation.test.ts
@@ -1,8 +1,9 @@
 import { DynamoClient } from 'dynamoClient';
 import type { CreateTableInput, ITableCreator } from 'tableCreator';
+import type { IItemReader, ReadItemsInput, ReadItemsResult } from 'itemReader';
 
 class TableCreatorStub implements ITableCreator {
-  calls: CreateTableInput[];
+  readonly calls: CreateTableInput[];
 
   constructor({}) {
     this.calls = [];
@@ -13,7 +14,26 @@ class TableCreatorStub implements ITableCreator {
   }
 }
 
-const getInput = (): CreateTableInput => ({
+class ItemReaderStub implements IItemReader {
+  readonly calls: ReadItemsInput[];
+  readonly result: Record<string, unknown>[];
+
+  constructor({
+    result = [{ test: 'test' }],
+  }: {
+    result?: Record<string, unknown>[];
+  }) {
+    this.calls = [];
+    this.result = result;
+  }
+
+  async readItems(input: ReadItemsInput): ReadItemsResult {
+    this.calls.push(input);
+    return new Promise((resolve) => resolve(this.result));
+  }
+}
+
+const getCreateTableInput = (): CreateTableInput => ({
   key: {
     partition: {
       name: 'partition',
@@ -27,6 +47,12 @@ const getInput = (): CreateTableInput => ({
   name: 'test',
 });
 
+const getReadItemsInput = (): ReadItemsInput => ({
+  partitionName: 'test',
+  partitionValue: 'test',
+  table: 'test',
+});
+
 test.each(['foo', 'bar'])(
   'create table forwards correct name',
   async (name) => {
@@ -34,7 +60,7 @@ test.each(['foo', 'bar'])(
     const client = new DynamoClient({
       tableCreator: stub,
     });
-    const input = getInput();
+    const input = getCreateTableInput();
     input.name = name;
     await client.createTable(input);
     expect(stub.calls[0].name).toBe(name);
@@ -48,7 +74,7 @@ test.each(['foo', 'bar'])(
     const client = new DynamoClient({
       tableCreator: stub,
     });
-    const input = getInput();
+    const input = getCreateTableInput();
     input.key.partition.name = name;
     await client.createTable(input);
     expect(stub.calls[0].key.partition.name).toBe(name);
@@ -62,7 +88,7 @@ test.each(['foo', 'bar'])(
     const client = new DynamoClient({
       tableCreator: stub,
     });
-    const input = getInput();
+    const input = getCreateTableInput();
     input.key.sort.name = name;
     await client.createTable(input);
     expect(stub.calls[0].key.sort.name).toBe(name);
@@ -76,7 +102,7 @@ test.each(['string', 'number', 'boolean'])(
     const client = new DynamoClient({
       tableCreator: stub,
     });
-    const input = getInput();
+    const input = getCreateTableInput();
     input.key.partition.type = type;
     await client.createTable(input);
     expect(stub.calls[0].key.partition.type).toBe(type);
@@ -90,7 +116,7 @@ test.each(['string', 'number', 'boolean'])(
     const client = new DynamoClient({
       tableCreator: stub,
     });
-    const input = getInput();
+    const input = getCreateTableInput();
     input.key.sort.type = type;
     await client.createTable(input);
     expect(stub.calls[0].key.sort.type).toBe(type);
@@ -102,8 +128,65 @@ test('create table correctly forwards input without sort', async () => {
   const client = new DynamoClient({
     tableCreator: stub,
   });
-  const input = getInput();
+  const input = getCreateTableInput();
   delete input.key.sort;
   await client.createTable(input);
   expect(stub.calls[0].key).not.toHaveProperty('sort');
 });
+
+test.each(['foo', 'bar'])(
+  'read items forwards correct partition value',
+  async (value) => {
+    const stub = new ItemReaderStub({});
+    const client = new DynamoClient({
+      itemReader: stub,
+    });
+    const input = getReadItemsInput();
+    input.partitionValue = value;
+    await client.readItems(input);
+    expect(stub.calls[0].partitionValue).toBe(value);
+  }
+);
+
+test.each(['foo', 'bar'])(
+  'read items forwards correct table',
+  async (value) => {
+    const stub = new ItemReaderStub({});
+    const client = new DynamoClient({
+      itemReader: stub,
+    });
+    const input = getReadItemsInput();
+    input.table = value;
+    await client.readItems(input);
+    expect(stub.calls[0].table).toBe(value);
+  }
+);
+
+test.each(['foo', 'bar'])(
+  'read items forwards correct partition name',
+  async (partitionName) => {
+    const stub = new ItemReaderStub({});
+    const client = new DynamoClient({
+      itemReader: stub,
+    });
+    const input = getReadItemsInput();
+    input.partitionName = partitionName;
+    await client.readItems(input);
+    expect(stub.calls[0].partitionName).toBe(partitionName);
+  }
+);
+
+test.each(['foo', 'bar'])(
+  'read items correctly returns ItemReader result',
+  async (value) => {
+    const stub = new ItemReaderStub({
+      result: [{ [value]: value }],
+    });
+    const client = new DynamoClient({
+      itemReader: stub,
+    });
+    const input = getReadItemsInput();
+    const result = await client.readItems(input);
+    expect(result).toStrictEqual([{ [value]: value }]);
+  }
+);
